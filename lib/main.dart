d7801d883b1a6a874a0ab146c7d99aa0d3c42bb1
Joel FERNANDES <joel.fernandes@github.com>
2026-03-23 23:40:19 +0100

    Mise en place d'un widget réutilisable pour movie card

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,39 @@
 import 'package:flutter/material.dart';
-import '../models/movie.dart';
-import '../services/tmdb_service.dart';
+import 'screens/main_screen.dart';
 
+import 'package:fernandes_joel_movie_explorer/models/movie.dart';
+import 'package:fernandes_joel_movie_explorer/services/tmdb_service.dart';
+import 'package:fernandes_joel_movie_explorer/widgets/movie_card.dart';
+
+//Point d'entrée de l'application
 void main() {
-  runApp(const MyApp());
+  runApp(const MovieExplorerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MovieExplorerApp extends StatelessWidget {
+  const MovieExplorerApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Movie Explorer',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainScreen> createState() => _MainScreen();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainScreen extends State<MainScreen> {
   final TmdbService _tmdbService = TmdbService();
   List<Movie> movies = [];
 
@@ -57,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary, 
-        title: Text(widget.title),
+        title: Text('Movie explorer'),
       ),
 
       body: ListView.separated(
@@ -65,86 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
         separatorBuilder: (context, index) => Divider(), 
         itemBuilder: (context, index) {
           final movie = movies[index];
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              children: [
-                Image.network(
-                  movie.posterUrl,
-                  width: 100,
-                  height: 150,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      width: 100,
-                      height: 150,
-                      color: Colors.grey[300],
-                      child: const Icon(
-                        Icons.movie,
-                        size: 40,
-                        color: Colors.grey,
-                      ),
-                    );
-                  },
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          movie.title,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          movie.releaseYear,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey[600],
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 20,
-                            ),
-                            const SizedBox(width: 4),
-                            Text(
-                              movie.voteAverage.toStringAsFixed(1),
-                              style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          movie.overview,
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[600],
-                          ),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
+          return MovieCard(movie: movie);
         }
       ),
     );
---
+++ lib/widgets/movie_card.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import  'package:fernandes_joel_movie_explorer/models/movie.dart';
+
+class MovieCard extends StatelessWidget {
+  final Movie movie;
+
+  const MovieCard({
+    super.key,
+    required this.movie,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        children: [
+          Image.network(
+            movie.posterUrl,
+            width: 100,
+            height: 150,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Container(
+                width: 100,
+                height: 150,
+                color: Colors.grey[300],
+                child: const Icon(
+                  Icons.movie,
+                  size: 40,
+                  color: Colors.grey,
+                ),
+              );
+            },
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    movie.releaseYear,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 20,
+                      ),
+                      const SizedBox(width: 4),
+                      Text(
+                        movie.voteAverage.toStringAsFixed(1),
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    movie.overview,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey[600],
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
